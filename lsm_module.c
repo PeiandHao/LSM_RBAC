35dc8f96114eb0194be13448e0e9c171110938df
peiandhao <2689271477@qq.com>
2024-03-25 17:24:26 +0800

    换中文了嘻嘻,更换了一个更加新的导出了加载钩子的内核,以及更新了一个白痴版本的lsmmodule

--- lsm_module.c
+++ lsm_module.c
@@ -13,11 +13,28 @@
 #include <linux/uidgid.h>
 #include <linux/cred.h>
 
+#define RBAC_CONFIG_PATH "/etc/config/"
+#define RBAC_USER_CONFIG "/etc/config/user_config"
+#define RBAC_ROLE_CONFIG "/etc/config/role_config"
 
+#define SYSCALL_MKDIR 1
+#define SYSCALL_RENAME 2
+#define SYSCALL_RMDIR 4
 
-int main()
-{
-    printf("Hello world\n");
+char role_list[3][10] = {"MKDIR\0", "RENAME\0", "RMDIR"\0};
+
+static int lsm_init(void){
+    printk(KERN_WARNING "[LSM_INIT]\n");
     return 0;
 }
 
+static void lsm_exit(void){
+    printk(KERN_WARNING "[LSM_EXIT]\n");
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("peiwithhao");
+MODULE_DESCRIPTION("My practice LSM based RBAC...");
+
+module_init(lsm_init);
+module_exit(lsm_exit);
